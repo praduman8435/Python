412e8900fdda505a2f93abceedb2af25e8579709
Praduman Prajapati <tech.praduman@gmail.com>
2023-02-10 21:58:08 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,76 @@
+# print anything
+print(5)
+print("Hello World")
+print('Python')
+print(12+43)
+print(10/3)
+print(10%2)
+print(43*3)
+
+
+
+#want to write a poem ? use this '\n'
+print('Twinkle Twinkle little Star,\nHow are wonder what you are!\nUp above the world so high,\nLike a diamond in the sky')
+
+
+
+# want to print this "" symbol use ''
+# want to print this '' symbol use ""
+print('You"re a Good One')
+print("You're a Good One")
+
+
+
+# you can print many data types at a time and seperate them by using this sep="" symbol
+print('Hello', 34.5, True, 66, sep="--->")
+print('Hello', 34.5, True, 66, sep="~~~")
+print('Hello', 34.5, True, 66, sep="<---->")
+
+
+
+# datatype
+a= 34               #int
+print(a)
+
+name="Praduman"     #string
+print(name)
+
+bool=True
+print(bool)         #boolean
+
+
+
+# want to know what type of data a variable is? use this
+print(type(a))
+print(type(name))
+print(type(bool))
+
+
+
+# want to make a complex no. ? use this
+n= complex(4, 5)
+u= complex(7, 9)
+print(n)
+print(u)
+
+
+
+# list datatype
+list= [4, 5.4, [-3, 6], "apple", ["fan", 'basketball'], True]           #lists are mutable datatype
+print(list)
+
+
+
+#tuple datatype
+tuple= ((2, 3), 56.4, "Rat", [True, 'Yak'], 34)                         #tuples are immutable
+print(tuple)
+
+
+
+# dictionery datatype
+dictionery={"animal":"Cow", "bird":"Parrot", "age":20, "bool":True}
+print(dictionery)
+
+
+
+#Did you know? In python everything is an Object
